835d6d86d3df0c94429699b1cc5fa361ba50a84b
Tom Kidd <macsourceports@gmail.com>
2021-10-15 15:49:09 -0500

    Some modifications to build process

---
+++ Quake/build_macos.sh
@@ -0,0 +1,245 @@
+#!/bin/bash
+unset ARM64_MACOSX_VERSION_MIN
+ARM64_MACOSX_VERSION_MIN="11.0"
+MACOSX_DEPLOYMENT_TARGET_ARM64="$ARM64_MACOSX_VERSION_MIN"
+VKQ1_VERSION="1.1"
+
+CURRENT_ARCH=$(uname -m)
+echo "CURRENT_ARCH: $CURRENT_ARCH"
+
+ICNSDIR="../Misc"
+ICNS="quake.icns"
+PRODUCT_NAME="vkQuake"
+WRAPPER_EXTENSION="app"
+WRAPPER_NAME="${PRODUCT_NAME}.${WRAPPER_EXTENSION}"
+CONTENTS_FOLDER_PATH="${WRAPPER_NAME}/Contents"
+UNLOCALIZED_RESOURCES_FOLDER_PATH="${CONTENTS_FOLDER_PATH}/Resources"
+EXECUTABLE_FOLDER_PATH="${CONTENTS_FOLDER_PATH}/MacOS"
+EXECUTABLE_NAME="vkquake"
+
+BUILT_PRODUCTS_DIR="build"
+PKGINFO="APPLVKQ1"
+
+# make the thing
+rm -rf "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}"
+make clean
+(ARCH=x86_64 make) || exit 1;
+# ARCH=arm64
+# make
+
+# here we go
+echo "Creating bundle '${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}'"
+
+# make the application bundle directories
+if [ ! -d "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}" ]; then
+	mkdir -p "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}" || exit 1;
+fi
+if [ ! -d "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}" ]; then
+	mkdir -p "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}" || exit 1;
+fi
+
+# copy and generate some application bundle resources
+cp vkquake "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
+
+cp /Users/tomkidd/Documents/GitHub/MSPStore/Cellar/sdl2/2.0.16/lib/libSDL2-2.0.0.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
+cp /Users/tomkidd/Documents/GitHub/MSPStore/Cellar/molten-vk/1.1.5/lib/libMoltenVK.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
+cp /Users/tomkidd/Documents/GitHub/MSPStore/Cellar/libvorbis/1.3.7/lib/libvorbisfile.3.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
+cp /Users/tomkidd/Documents/GitHub/MSPStore/Cellar/libvorbis/1.3.7/lib/libvorbis.0.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
+cp /Users/tomkidd/Documents/GitHub/MSPStore/Cellar/libogg/1.3.5/lib/libogg.0.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
+cp /Users/tomkidd/Documents/GitHub/MSPStore/Cellar/mad/0.15.1b/lib/libmad.0.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
+
+cp ${ICNSDIR}/${ICNS} "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/$ICNS" || exit 1;
+echo -n ${PKGINFO} > "${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/PkgInfo" || exit 1;
+
+# use install_name tool to point executable to bundled resources (probably wrong long term way to do it)
+# if [ "$CURRENT_ARCH" == "x86_64" ]; then
+	#modify vkquake
+	install_name_tool -change /usr/local/opt/sdl2/lib/libSDL2-2.0.0.dylib @executable_path/libSDL2-2.0.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /usr/local/opt/molten-vk/lib/libMoltenVK.dylib @executable_path/libMoltenVK.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /usr/local/opt/libvorbis/lib/libvorbisfile.3.dylib @executable_path/libvorbisfile.3.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /usr/local/opt/libvorbis/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /usr/local/opt/mad/lib/libmad.0.dylib @executable_path/libmad.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	
+	#modify libvorbisfile
+	install_name_tool -change /usr/local/Cellar/libvorbis/1.3.7/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbisfile.3.dylib
+	install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbisfile.3.dylib
+	
+	#modify libvorbis
+	install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbis.0.dylib
+# else
+	#modify vkquake
+	install_name_tool -change /opt/homebrew/opt/sdl2/lib/libSDL2-2.0.0.dylib @executable_path/libSDL2-2.0.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /opt/homebrew/opt/molten-vk/lib/libMoltenVK.dylib @executable_path/libMoltenVK.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /opt/homebrew/opt/libvorbis/lib/libvorbisfile.3.dylib @executable_path/libvorbisfile.3.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /opt/homebrew/opt/libvorbis/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /opt/homebrew/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	install_name_tool -change /opt/homebrew/opt/mad/lib/libmad.0.dylib @executable_path/libmad.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
+	
+	#modify libvorbisfile
+	install_name_tool -change /opt/homebrew/Cellar/libvorbis/1.3.7/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbisfile.3.dylib
+	install_name_tool -change /opt/homebrew/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbisfile.3.dylib
+	
+	#modify libvorbis
+	install_name_tool -change /opt/homebrew/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbis.0.dylib
+# fi 
+# create Info.Plist
+PLIST="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+    <key>CFBundleDevelopmentRegion</key>
+    <string>en</string>
+    <key>CFBundleExecutable</key>
+    <string>${EXECUTABLE_NAME}</string>
+    <key>CFBundleIconFile</key>
+    <string>quake</string>
+    <key>CFBundleIdentifier</key>
+    <string>com.macsourceports.${PRODUCT_NAME}</string>
+    <key>CFBundleInfoDictionaryVersion</key>
+    <string>6.0</string>
+    <key>CFBundleName</key>
+    <string>${PRODUCT_NAME}</string>
+    <key>CFBundlePackageType</key>
+    <string>APPL</string>
+    <key>CFBundleShortVersionString</key>
+    <string>${VKQ1_VERSION}</string>
+    <key>CFBundleSignature</key>
+    <string>????</string>
+    <key>CFBundleVersion</key>
+    <string>${VKQ1_VERSION}</string>
+    <key>CGDisableCoalescedUpdates</key>
+    <true/>
+    <key>LSMinimumSystemVersion</key>
+    <string>${MACOSX_DEPLOYMENT_TARGET}</string>"
+
+if [ -n "${MACOSX_DEPLOYMENT_TARGET_X86_64}" ] || [ -n "${MACOSX_DEPLOYMENT_TARGET_ARM64}" ]; then
+	PLIST="${PLIST}
+    <key>LSMinimumSystemVersionByArchitecture</key>
+    <dict>"
+
+	if [ -n "${MACOSX_DEPLOYMENT_TARGET_X86_64}" ]; then
+	PLIST="${PLIST}
+        <key>x86_64</key>
+        <string>${MACOSX_DEPLOYMENT_TARGET_X86_64}</string>"
+	fi
+	
+	if [ -n "${MACOSX_DEPLOYMENT_TARGET_ARM64}" ]; then
+	PLIST="${PLIST}
+        <key>arm64</key>
+        <string>${MACOSX_DEPLOYMENT_TARGET_ARM64}</string>"
+	fi
+
+	PLIST="${PLIST}
+    </dict>"
+fi
+
+PLIST="${PLIST}
+    <key>NSHumanReadableCopyright</key>
+    <string>QUAKE Copyright © 1996-2021 id Software, Inc. All rights reserved.</string>
+    <key>NSPrincipalClass</key>
+    <string>NSApplication</string>
+    <key>NSHighResolutionCapable</key>
+    <false/>
+    <key>NSRequiresAquaSystemAppearance</key>
+    <false/>
+</dict>
+</plist>
+"
+echo -e "${PLIST}" > "${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/Info.plist"
+
+echo "bundle done."
+
+# user-specific values
+# specify the actual values in a separate file called build_macosx_values.sh
+
+# ****************************************************************************************
+# identity as specified in Keychain
+SIGNING_IDENTITY="Developer ID Application: Your Name (XXXXXXXXX)"
+
+ASC_USERNAME="your@apple.id"
+
+# signing password is app-specific (https://appleid.apple.com/account/manage) and stored in Keychain (as "notarize-app" in this case)
+ASC_PASSWORD="@keychain:notarize-app"
+
+# ProviderShortname can be found with
+# xcrun altool --list-providers -u your@apple.id -p "@keychain:notarize-app"
+ASC_PROVIDER="XXXXXXXXX"
+# ****************************************************************************************
+
+source build_macosx_values.sh
+
+# release build location
+RELEASE_LOCATION="build/release-darwin-universal2"
+
+# release build name
+RELEASE_BUILD="ioquake3.app"
+
+# Pre-notarized zip file (not what is shipped)
+PRE_NOTARIZED_ZIP="vkquake_prenotarized.zip"
+
+# Post-notarized zip file (shipped)
+POST_NOTARIZED_ZIP="vkquake_notarized.zip"
+
+BUNDLE_ID="com.macsourceports.vkquake3"
+
+# sign the resulting app bundle
+echo "signing..."
+# codesign --force --options runtime --deep --entitlements "${ENTITLEMENTS_FILE}" --sign "${SIGNING_IDENTITY}" ${RELEASE_LOCATION}/${RELEASE_BUILD}	# sign the resulting app bundle
+codesign --force --options runtime --deep --sign "${SIGNING_IDENTITY}" ${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}
+
+if [ "$1" == "notarize" ]; then
+
+    cd ${BUILT_PRODUCTS_DIR}
+
+	# notarize app
+	# script taken from https://github.com/rednoah/notarize-app
+
+	# create the zip to send to the notarization service
+	echo "zipping..."
+	ditto -c -k --sequesterRsrc --keepParent ${WRAPPER_NAME} ${PRE_NOTARIZED_ZIP}
+
+	# create temporary files
+	NOTARIZE_APP_LOG=$(mktemp -t notarize-app)
+	NOTARIZE_INFO_LOG=$(mktemp -t notarize-info)
+
+	# delete temporary files on exit
+	function finish {
+		rm "$NOTARIZE_APP_LOG" "$NOTARIZE_INFO_LOG"
+	}
+	trap finish EXIT
+
+	echo "submitting..."
+	# submit app for notarization
+	if xcrun altool --notarize-app --primary-bundle-id "$BUNDLE_ID" --asc-provider "$ASC_PROVIDER" --username "$ASC_USERNAME" --password "$ASC_PASSWORD" -f "$PRE_NOTARIZED_ZIP" > "$NOTARIZE_APP_LOG" 2>&1; then
+		cat "$NOTARIZE_APP_LOG"
+		RequestUUID=$(awk -F ' = ' '/RequestUUID/ {print $2}' "$NOTARIZE_APP_LOG")
+
+		# check status periodically
+		while sleep 60 && date; do
+			# check notarization status
+			if xcrun altool --notarization-info "$RequestUUID" --asc-provider "$ASC_PROVIDER" --username "$ASC_USERNAME" --password "$ASC_PASSWORD" > "$NOTARIZE_INFO_LOG" 2>&1; then
+				cat "$NOTARIZE_INFO_LOG"
+
+				# once notarization is complete, run stapler and exit
+				if ! grep -q "Status: in progress" "$NOTARIZE_INFO_LOG"; then
+					xcrun stapler staple "$WRAPPER_NAME"
+					break
+				fi
+			else
+				cat "$NOTARIZE_INFO_LOG" 1>&2
+				exit 1
+			fi
+		done
+	else
+		cat "$NOTARIZE_APP_LOG" 1>&2
+		exit 1
+	fi
+
+	echo "notarized"
+	echo "zipping notarized..."
+
+	ditto -c -k --sequesterRsrc --keepParent ${WRAPPER_NAME} ${POST_NOTARIZED_ZIP}
+
+	echo "done. ${POST_NOTARIZED_ZIP} contains notarized ${WRAPPER_NAME} build."
+fi
