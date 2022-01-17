d227039f786b1844969cf46fbf99d75018b663d4
Tom Kidd <macsourceports@gmail.com>
2022-01-16 21:41:00 -0600

    Revamped build scripts to fit new paradigm
    Still not sure why the thing doesn't launch when downloaded off internet

--- macsourceports_universal2.sh
+++ macsourceports_universal2.sh
@@ -1,11 +1,11 @@
 # game/app specific values
-export APP_VERSION="1.11.0"
+export APP_VERSION="1.12.2"
 export ICONSDIR="Misc"
-export ICONSFILENAME="quake.icns"
+export ICONSFILENAME="quake"
 export PRODUCT_NAME="vkQuake"
 export EXECUTABLE_NAME="vkquake"
 export PKGINFO="APPLVKQ1"
-export COPYRIGHT_TEXT="Return to Castle Wolfenstein Copyright © 1999-2000 id Software, Inc. All rights reserved."
+export COPYRIGHT_TEXT="QUAKE Copyright © 1996-2021 id Software, Inc. All rights reserved."
 
 # constants
 export BUILT_PRODUCTS_DIR="release"
@@ -24,133 +24,48 @@ NCPU=`sysctl -n hw.ncpu`
 
 # make the thing
 
-# giving up custom build dirs for now
-
-# if [ -d build ]; then
-# rm -rf build
-# fi
-# mkdir build
-# cd build
-
 # rm -rf "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}"
-# if [ ! -d "x86_64" ]; then
-# 	mkdir -p "x86_64" || exit 1;
-# fi
-# if [ ! -d "arm64" ]; then
-# 	mkdir -p "arm64" || exit 1;
-# fi
 
-cd ../Quake
+cd Quake
+rm -rf "x86_64"
+mkdir -p "x86_64" || exit 1;
+rm -rf "arm64"
+mkdir -p "arm64" || exit 1;
+
 make clean
 (ARCH=x86_64 make -j$NCPU) || exit 1;
 (ARCH=arm64 make -j$NCPU) || exit 1;
 
+cd x86_64
+mkdir -p "${EXECUTABLE_FOLDER_PATH}"
+cp "${EXECUTABLE_NAME}" "${EXECUTABLE_FOLDER_PATH}"
+echo dylibbundler -od -b -x ./"${EXECUTABLE_FOLDER_PATH}"/"${EXECUTABLE_NAME}" -d ./"${EXECUTABLE_FOLDER_PATH}"/libs-x86_64/ -p @executable_path/libs-x86_64/
+dylibbundler -od -b -x ./"${EXECUTABLE_FOLDER_PATH}"/"${EXECUTABLE_NAME}" -d ./"${EXECUTABLE_FOLDER_PATH}"/libs-x86_64/ -p @executable_path/libs-x86_64/
+cd ..
+
+cd arm64
+mkdir -p "${EXECUTABLE_FOLDER_PATH}"
+cp "${EXECUTABLE_NAME}" "${EXECUTABLE_FOLDER_PATH}"
+echo dylibbundler -od -b -x ./"${EXECUTABLE_FOLDER_PATH}"/"${EXECUTABLE_NAME}" -d ./"${EXECUTABLE_FOLDER_PATH}"/libs-arm64/ -p @executable_path/libs-arm64/
+dylibbundler -od -b -x ./"${EXECUTABLE_FOLDER_PATH}"/"${EXECUTABLE_NAME}" -d ./"${EXECUTABLE_FOLDER_PATH}"/libs-arm64/ -p @executable_path/libs-arm64/
+cd ..
+
+cd ..
+
 # here we go
 echo "Creating bundle '${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}'"
 
-# make the application bundle directories
-cd ..
-if [ ! -d "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}" ]; then
-	mkdir -p "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}" || exit 1;
-fi
-if [ ! -d "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}" ]; then
-	mkdir -p "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}" || exit 1;
-fi
+# create the app bundle
+"../MSPScripts/build_app_bundle.sh"
 
 # copy and generate some application bundle resources
-echo lipo Quake/x86_64/vkquake Quake/arm64/vkquake -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/vkquake" -create
-lipo Quake/x86_64/vkquake Quake/arm64/vkquake -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/vkquake" -create
+lipo Quake/x86_64/"${EXECUTABLE_FOLDER_PATH}"/vkquake Quake/arm64/"${EXECUTABLE_FOLDER_PATH}"/vkquake -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/vkquake" -create
 
-cp ~/Documents/GitHub/MSPStore/lib/libSDL2-2.0.0.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
-cp ~/Documents/GitHub/MSPStore/lib/libMoltenVK.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
-cp ~/Documents/GitHub/MSPStore/lib/libvorbisfile.3.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
-cp ~/Documents/GitHub/MSPStore/lib/libvorbis.0.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
-cp ~/Documents/GitHub/MSPStore/lib/libogg.0.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
-cp ~/Documents/GitHub/MSPStore/lib/libmad.0.dylib "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}"
+mkdir "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libs-x86_64"
+cp -a "Quake/x86_64/${EXECUTABLE_FOLDER_PATH}/libs-x86_64/." "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libs-x86_64"
 
-echo cp ${ICONSDIR}/${ICONSFILENAME} "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/" || exit 1;
-cp ${ICONSDIR}/${ICONSFILENAME} "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/" || exit 1;
-echo -n ${PKGINFO} > "${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/PkgInfo" || exit 1;
-
-# use install_name tool to point executable to bundled resources (probably wrong long term way to do it)
-#modify vkquake x86_64
-install_name_tool -change /usr/local/opt/sdl2/lib/libSDL2-2.0.0.dylib @executable_path/libSDL2-2.0.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /usr/local/opt/molten-vk/lib/libMoltenVK.dylib @executable_path/libMoltenVK.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /usr/local/opt/libvorbis/lib/libvorbisfile.3.dylib @executable_path/libvorbisfile.3.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /usr/local/opt/libvorbis/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /usr/local/opt/mad/lib/libmad.0.dylib @executable_path/libmad.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-
-#modify libvorbisfile x86_64
-install_name_tool -change /usr/local/Cellar/libvorbis/1.3.7/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbisfile.3.dylib
-install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbisfile.3.dylib
-
-#modify libvorbis x86_64
-install_name_tool -change /usr/local/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbis.0.dylib
-
-#modify vkquake arm64
-install_name_tool -change /opt/homebrew/opt/sdl2/lib/libSDL2-2.0.0.dylib @executable_path/libSDL2-2.0.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /opt/homebrew/opt/molten-vk/lib/libMoltenVK.dylib @executable_path/libMoltenVK.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /opt/homebrew/opt/libvorbis/lib/libvorbisfile.3.dylib @executable_path/libvorbisfile.3.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /opt/homebrew/opt/libvorbis/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /opt/homebrew/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-install_name_tool -change /opt/homebrew/opt/mad/lib/libmad.0.dylib @executable_path/libmad.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/${EXECUTABLE_NAME}
-
-#modify libvorbisfile arm64
-install_name_tool -change /opt/homebrew/Cellar/libvorbis/1.3.7/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbisfile.3.dylib
-install_name_tool -change /opt/homebrew/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbisfile.3.dylib
-
-#modify libvorbis arm64
-install_name_tool -change /opt/homebrew/opt/libogg/lib/libogg.0.dylib @executable_path/libogg.0.dylib ${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libvorbis.0.dylib
-
-# create Info.Plist
-PLIST="<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
-<plist version=\"1.0\">
-<dict>
-    <key>CFBundleDevelopmentRegion</key>
-    <string>en</string>
-    <key>CFBundleExecutable</key>
-    <string>${EXECUTABLE_NAME}</string>
-    <key>CFBundleIconFile</key>
-    <string>quake</string>
-    <key>CFBundleIdentifier</key>
-    <string>com.macsourceports.${PRODUCT_NAME}</string>
-    <key>CFBundleInfoDictionaryVersion</key>
-    <string>6.0</string>
-    <key>CFBundleName</key>
-    <string>${PRODUCT_NAME}</string>
-    <key>CFBundlePackageType</key>
-    <string>APPL</string>
-    <key>CFBundleShortVersionString</key>
-    <string>${APP_VERSION}</string>
-    <key>CFBundleSignature</key>
-    <string>????</string>
-    <key>CFBundleVersion</key>
-    <string>${APP_VERSION}</string>
-    <key>CGDisableCoalescedUpdates</key>
-    <true/>
-    <key>LSMinimumSystemVersion</key>
-    <string>10.7</string>
-    <key>LSMinimumSystemVersionByArchitecture</key>
-    <dict>
-        <key>x86_64</key>
-        <string>10.7</string>
-        <key>arm64</key>
-        <string>11.0</string>
-    </dict>
-	<key>NSHumanReadableCopyright</key>
-    <string>QUAKE Copyright © 1996-2021 id Software, Inc. All rights reserved.</string>
-    <key>NSPrincipalClass</key>
-    <string>NSApplication</string>
-    <key>NSHighResolutionCapable</key>
-    <false/>
-    <key>NSRequiresAquaSystemAppearance</key>
-    <false/>
-</dict>
-</plist>
-"
-echo "${PLIST}" > "${BUILT_PRODUCTS_DIR}/${CONTENTS_FOLDER_PATH}/Info.plist"
+mkdir "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libs-arm64"
+cp -a "Quake/arm64/${EXECUTABLE_FOLDER_PATH}/libs-arm64/." "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libs-arm64"
 
 echo "bundle done."
 
