45eed400041dd1995590a5cabce65de1001f6b1a
Tom Kidd <macsourceports@gmail.com>
2022-02-05 18:51:35 -0600

    Refinements to build process

--- macsourceports_universal2.sh
+++ macsourceports_universal2.sh
@@ -7,66 +7,31 @@ export EXECUTABLE_NAME="vkquake"
 export PKGINFO="APPLVKQ1"
 export COPYRIGHT_TEXT="QUAKE Copyright © 1996-2021 id Software, Inc. All rights reserved."
 
-# constants
-export BUILT_PRODUCTS_DIR="release"
-export WRAPPER_NAME="${PRODUCT_NAME}.app"
-export CONTENTS_FOLDER_PATH="${WRAPPER_NAME}/Contents"
-export EXECUTABLE_FOLDER_PATH="${CONTENTS_FOLDER_PATH}/MacOS"
-export UNLOCALIZED_RESOURCES_FOLDER_PATH="${CONTENTS_FOLDER_PATH}/Resources"
-export ICONS="${ICONSFILENAME}.icns"
-export BUNDLE_ID="com.macsourceports.${PRODUCT_NAME}"
+#constants
+source ../MSPScripts/constants.sh
 
-CURRENT_ARCH=$(uname -m)
-echo "CURRENT_ARCH: $CURRENT_ARCH"
+rm -rf ${BUILT_PRODUCTS_DIR}
 
-# For parallel make on multicore boxes...
-NCPU=`sysctl -n hw.ncpu`
-
-# make the thing
-
-# rm -rf "${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}"
+rm -rf ${X86_64_BUILD_FOLDER}
+mkdir ${X86_64_BUILD_FOLDER}
+rm -rf ${ARM64_BUILD_FOLDER}
+mkdir ${ARM64_BUILD_FOLDER}
 
 cd Quake
-rm -rf "x86_64"
-mkdir -p "x86_64" || exit 1;
-rm -rf "arm64"
-mkdir -p "arm64" || exit 1;
-
 make clean
 (ARCH=x86_64 make -j$NCPU) || exit 1;
+cd ..
+mkdir -p ${X86_64_BUILD_FOLDER}/"${EXECUTABLE_FOLDER_PATH}"
+mv Quake/"${EXECUTABLE_NAME}" ${X86_64_BUILD_FOLDER}/"${EXECUTABLE_FOLDER_PATH}"
+
+cd Quake
+make clean
 (ARCH=arm64 make -j$NCPU) || exit 1;
-
-cd x86_64
-mkdir -p "${EXECUTABLE_FOLDER_PATH}"
-cp "${EXECUTABLE_NAME}" "${EXECUTABLE_FOLDER_PATH}"
-echo dylibbundler -od -b -x ./"${EXECUTABLE_FOLDER_PATH}"/"${EXECUTABLE_NAME}" -d ./"${EXECUTABLE_FOLDER_PATH}"/libs-x86_64/ -p @executable_path/libs-x86_64/
-dylibbundler -od -b -x ./"${EXECUTABLE_FOLDER_PATH}"/"${EXECUTABLE_NAME}" -d ./"${EXECUTABLE_FOLDER_PATH}"/libs-x86_64/ -p @executable_path/libs-x86_64/
 cd ..
-
-cd arm64
-mkdir -p "${EXECUTABLE_FOLDER_PATH}"
-cp "${EXECUTABLE_NAME}" "${EXECUTABLE_FOLDER_PATH}"
-echo dylibbundler -od -b -x ./"${EXECUTABLE_FOLDER_PATH}"/"${EXECUTABLE_NAME}" -d ./"${EXECUTABLE_FOLDER_PATH}"/libs-arm64/ -p @executable_path/libs-arm64/
-dylibbundler -od -b -x ./"${EXECUTABLE_FOLDER_PATH}"/"${EXECUTABLE_NAME}" -d ./"${EXECUTABLE_FOLDER_PATH}"/libs-arm64/ -p @executable_path/libs-arm64/
-cd ..
-
-cd ..
-
-# here we go
-echo "Creating bundle '${BUILT_PRODUCTS_DIR}/${WRAPPER_NAME}'"
+mkdir -p ${ARM64_BUILD_FOLDER}/"${EXECUTABLE_FOLDER_PATH}"
+mv Quake/"${EXECUTABLE_NAME}" ${ARM64_BUILD_FOLDER}/"${EXECUTABLE_FOLDER_PATH}"
 
 # create the app bundle
 "../MSPScripts/build_app_bundle.sh"
-
-# copy and generate some application bundle resources
-lipo Quake/x86_64/"${EXECUTABLE_FOLDER_PATH}"/vkquake Quake/arm64/"${EXECUTABLE_FOLDER_PATH}"/vkquake -output "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/vkquake" -create
-
-mkdir "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libs-x86_64"
-cp -a "Quake/x86_64/${EXECUTABLE_FOLDER_PATH}/libs-x86_64/." "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libs-x86_64"
-
-mkdir "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libs-arm64"
-cp -a "Quake/arm64/${EXECUTABLE_FOLDER_PATH}/libs-arm64/." "${BUILT_PRODUCTS_DIR}/${EXECUTABLE_FOLDER_PATH}/libs-arm64"
-
-echo "bundle done."
 
 "../MSPScripts/sign_and_notarize.sh" "$1"
